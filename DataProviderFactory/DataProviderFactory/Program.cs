7ae7bc70772d237cdeee6a79b37d7678c20ca3b4
m_a_ks_ <maksim.m00@mail.ru>
2021-01-10 17:44:06 +0300

    Data Provider Factories

---
+++ DataProviderFactory/DataProviderFactory/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Configuration;
+using System.Data.Common;
+
+namespace DataProviderFactory
+{
+    internal class Program
+    {
+        private static void ShowError(string objectName)
+        {
+            WriteLine($"There was an issue creating the {objectName}");
+            ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLine("*** Fun project with Data Provider Factories ***\n");
+
+            string dataProvider = ConfigurationManager.AppSettings["provider"];
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+
+            DbProviderFactory factory = DbProviderFactories.GetFactory(dataProvider);
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if (connection == null)
+                {
+                    ShowError("Connection");
+                    return;
+                }
+                WriteLine($"Your connection object is a: {connection.GetType().Name}");
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                DbCommand command = factory.CreateCommand();
+                if (command == null)
+                {
+                    ShowError("Command");
+                    return;
+                }
+                WriteLine($"Your command object is a: {command.GetType().Name}");
+                command.Connection = connection;
+                command.CommandText = "Select * From Inventory";
+
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    WriteLine($"Your data reader object is a: {dataReader.GetType().Name}");
+                    WriteLine("\n*** Current Inventory ***");
+                    while (dataReader.Read())
+                        WriteLine($"-> Car #{dataReader["CarId"] } is a {dataReader["Make"]}.");
+                }
+            }
+            ReadKey();
+            
+        }
+    }
+}
